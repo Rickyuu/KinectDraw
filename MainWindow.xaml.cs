c5438b5b0c9b807629420acf6a3096258506517a
rickyuu <rickyliang92@gmail.com>
2014-11-15 20:53:17 +0800

    2D with point of r=50(may crash because of high demand of RAM)

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace KinectDrawDotsGame
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor kinectDevice;
+        private Skeleton[] frameSkeletons;
+        private DotPuzzle puzzle;
+
+        public KinectSensor KinectDevice
+        {
+            get { return this.kinectDevice; }
+            set
+            {
+                if (this.kinectDevice != value)
+                {
+                    //Uninitialize
+                    if (this.kinectDevice != null)
+                    {
+                        this.kinectDevice.Stop();
+                        this.kinectDevice.SkeletonFrameReady -= KinectDevice_SkeletonFrameReady;
+                        this.kinectDevice.SkeletonStream.Disable();
+                        this.frameSkeletons = null;
+                    }
+
+                    this.kinectDevice = value;
+
+                    //Initialize
+                    if (this.kinectDevice != null)
+                    {
+                        if (this.kinectDevice.Status == KinectStatus.Connected)
+                        {
+                            this.kinectDevice.SkeletonStream.Enable();
+                            this.frameSkeletons = new Skeleton[this.kinectDevice.SkeletonStream.FrameSkeletonArrayLength];
+                            this.kinectDevice.Start();
+                            this.KinectDevice.SkeletonFrameReady += KinectDevice_SkeletonFrameReady;
+                        }
+                    }
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            puzzle = new DotPuzzle();
+
+            this.Loaded += (s, e) =>
+            {
+                KinectSensor.KinectSensors.StatusChanged += KinectSensors_StatusChanged;
+                this.KinectDevice = KinectSensor.KinectSensors.FirstOrDefault(x => x.Status == KinectStatus.Connected);
+            };
+        }
+
+        private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case KinectStatus.Initializing:
+                case KinectStatus.Connected:
+                case KinectStatus.NotPowered:
+                case KinectStatus.NotReady:
+                case KinectStatus.DeviceNotGenuine:
+                    this.KinectDevice = e.Sensor;
+                    break;
+                case KinectStatus.Disconnected:
+                    //TODO: Give the user feedback to plug-in a Kinect device.                    
+                    this.KinectDevice = null;
+                    break;
+                default:
+                    //TODO: Show an error state
+                    break;
+            }
+        }
+
+        private void KinectDevice_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame frame = e.OpenSkeletonFrame())
+            {
+                if (frame != null)
+                {
+                    frame.CopySkeletonDataTo(this.frameSkeletons);
+                    Skeleton skeleton = GetPrimarySkeleton(this.frameSkeletons);
+
+                    Skeleton[] dataSet2 = new Skeleton[this.frameSkeletons.Length];
+                    frame.CopySkeletonDataTo(dataSet2);
+
+                    if (skeleton == null)
+                    {
+                        HandCursorElement.Visibility = Visibility.Collapsed;
+                    }
+                    else
+                    {
+                        Joint primaryHand = GetPrimaryHand(skeleton);
+                        TrackHand(primaryHand);
+                        TrackPuzzle(primaryHand.Position);
+                    }
+                }
+            }
+        }
+
+        private static Skeleton GetPrimarySkeleton(Skeleton[] skeletons)
+        {
+            Skeleton skeleton = null;
+
+            if (skeletons != null)
+            {
+                //查找最近的游戏者
+                for (int i = 0; i < skeletons.Length; i++)
+                {
+                    if (skeletons[i].TrackingState == SkeletonTrackingState.Tracked)
+                    {
+                        if (skeleton == null)
+                        {
+                            skeleton = skeletons[i];
+                        }
+                        else
+                        {
+                            if (skeleton.Position.Z > skeletons[i].Position.Z)
+                            {
+                                skeleton = skeletons[i];
+                            }
+                        }
+                    }
+                }
+            }
+
+            return skeleton;
+        }
+
+        private static Joint GetPrimaryHand(Skeleton skeleton)
+        {
+            Joint primaryHand = new Joint();
+
+            if (skeleton != null)
+            {
+                primaryHand = skeleton.Joints[JointType.HandLeft];
+                Joint righHand = skeleton.Joints[JointType.HandRight];
+
+
+                if (righHand.TrackingState != JointTrackingState.NotTracked)
+                {
+                    if (primaryHand.TrackingState == JointTrackingState.NotTracked)
+                    {
+                        primaryHand = righHand;
+                    }
+                    else
+                    {
+                        if (primaryHand.Position.Z > righHand.Position.Z)
+                        {
+                            primaryHand = righHand;
+                        }
+                    }
+                }
+            }
+
+            return primaryHand;
+        }
+
+        private void TrackHand(Joint hand)
+        {
+            if (hand.TrackingState == JointTrackingState.NotTracked)
+            {
+                HandCursorElement.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                HandCursorElement.Visibility = Visibility.Visible;
+
+
+                DepthImagePoint point = this.kinectDevice.MapSkeletonPointToDepth(hand.Position, this.kinectDevice.DepthStream.Format);
+                point.X = (int)((point.X * LayoutRoot.ActualWidth / kinectDevice.DepthStream.FrameWidth) - (HandCursorElement.ActualWidth / 2.0));
+                point.Y = (int)((point.Y * LayoutRoot.ActualHeight / kinectDevice.DepthStream.FrameHeight) - (HandCursorElement.ActualHeight / 2.0));
+
+                Canvas.SetLeft(HandCursorElement, point.X);
+                Canvas.SetTop(HandCursorElement, point.Y);
+
+                if (hand.JointType == JointType.HandRight)
+                {
+                    HandCursorScale.ScaleX = 1;
+                }
+                else
+                {
+                    HandCursorScale.ScaleX = -1;
+                }
+            }
+        }
+
+        private void DrawPuzzle(DotPuzzle puzzle)
+        {
+            PuzzleBoardElement.Children.Clear();
+
+            if (puzzle != null)
+            {
+                for (int i = 0; i < puzzle.Dots.Count; i++)
+                {
+                    Grid dotContainer = new Grid();
+                    dotContainer.Width = 50;
+                    dotContainer.Height = 50;
+                    dotContainer.Children.Add(new Ellipse { Fill = Brushes.Gray });
+
+                    //在UI界面上绘制点
+                    Canvas.SetTop(dotContainer, puzzle.Dots[i].Y - (dotContainer.Height / 2));
+                    Canvas.SetLeft(dotContainer, puzzle.Dots[i].X - (dotContainer.Width / 2));
+                    PuzzleBoardElement.Children.Add(dotContainer);
+                }
+            }
+        }
+
+        private void TrackPuzzle(SkeletonPoint position)
+        {
+                DepthImagePoint point = this.kinectDevice.MapSkeletonPointToDepth(position, kinectDevice.DepthStream.Format);
+                point.X = (int)(point.X * LayoutRoot.ActualWidth / kinectDevice.DepthStream.FrameWidth);
+                point.Y = (int)(point.Y * LayoutRoot.ActualHeight / kinectDevice.DepthStream.FrameHeight);
+                Point handPoint = new Point(point.X, point.Y);
+
+                this.puzzle.Dots.Add(handPoint);
+                DrawPuzzle(this.puzzle);
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+This is a C# project.
+
+We use kinect to collect 3D data for us and try to draw the picture on the screen. And finally we will save the picture trace and try to use a 3D printer to print the model for us.
+
+The main steps of this project are as follows:
+1. draw in 2D without high demanding of memory
+2. try to show the picture with a 3D effect
+3. try to save the 3D file
